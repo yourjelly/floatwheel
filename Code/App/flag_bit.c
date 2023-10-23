5cd9e7a16b73e4b07ff4ab15c2c38c1c4e90ed14
Xuereb <jonxuereb@gmail.com>
2023-10-22 23:34:21 -0400

    Grouping Flags and Functions by purpose

--- Code/App/flag_bit.c
+++ Code/App/flag_bit.c
@@ -1,5 +1,7 @@
 #include "flag_bit.h"
 
+//// BOARD STATE
+
 /*
 	Power_Flag = 0; Default state.
 	Power_Flag = 1; VESC booting up.
@@ -15,6 +17,15 @@ uint8_t Power_Flag = 0;
 */
 uint8_t Charge_Flag = 0;
 /*
+	Usart_Flag = 0; Default state.
+	Usart_Flag = 1; Parsing successful.
+	Usart_Flag = 2; Parsing failed or waiting for the next parsing.
+*/
+uint8_t Usart_Flag = 0;
+
+//// LIGHTS STATE
+
+/*
 	Flashlight_Flag = 0; Default state.
 	Flashlight_Flag = 1; VESC booting up.
 	Flashlight_Flag = 2; Front light white, rear light red (forward).
@@ -23,9 +34,16 @@ uint8_t Charge_Flag = 0;
 */
 uint8_t Flashlight_Flag = 0;
 /*
-	Brightness_Adjustment_Flag = 0; Default state.
-	Brightness_Adjustment_Flag = 1; Starting brightness adjustment.
-	Brightness_Adjustment_Flag = 2; Brightness adjustment completed.
+	Light_Profile = 0; Default state.
+	Light_Profile = 1; Headlight bightness low, Lightpad brightness high, beep once
+	Light_Profile = 2; Headlight bightness mid, Lightpad brightness mid, beep twice
+	Light_Profile = 3; Headlight bightness high, Lightpad brightness low, beep thrice
+*/
+uint8_t Light_Profile = 0;
+/*
+	Brightness_Adjustment_Adjustment_Flag = 0; Default state.
+	Brightness_Adjustment_Adjustment_Flag = 1; Starting brightness adjustment.
+	Brightness_Adjustment_Adjustment_Flag = 2; Brightness adjustment completed.
 */
 uint8_t Brightness_Adjustment_Flag = 0;
 /*
@@ -42,6 +60,9 @@ uint8_t Lightbar_Battery_Flag = 0;
 	Sensor_Activation_Display_Flag = 4; All 10 LEDs off.
 */
 uint8_t Sensor_Activation_Display_Flag = 0;
+
+uint8_t Lightbar_Brightness = 0; // Lightbar runtime brightness setting (0-255)
+
 /*
 	Power_Display_Flag = 0; Default state.
 	Power_Display_Flag = 1; Full charge: 10 white LEDs.
@@ -56,61 +77,29 @@ uint8_t Sensor_Activation_Display_Flag = 0;
 	Power_Display_Flag = 10; 10%: 1 red LED.
 */
 uint8_t Power_Display_Flag = 0;
+
+//// FOOTPAD STATE
+
+uint16_t ADC_Time = 0; // ADC sampling time.
+
+float ADC1_Val = 0; // ADC1
+
+float ADC2_Val = 0; // ADC2
+
+//// BUZZER STATE
+
 /*
 	Buzzer_Flag = 0; Default state.
 	Buzzer_Flag = 1; Buzzer disabled.
 	Buzzer_Flag = 2; Buzzer enabled.
 */
 uint8_t Buzzer_Flag = 0;
-/*
-	Usart_Flag = 0; Default state.
-	Usart_Flag = 1; Parsing successful.
-	Usart_Flag = 2; Parsing failed or waiting for the next parsing.
-*/
-uint8_t Usart_Flag = 0;
 
 uint16_t Buzzer_Time = 0; // Buzzer sounding time.
 
-uint16_t Charge_Time = 0; // Charging time.
-
-uint16_t Flashlight_Time = 0; // Flashlight time.
-
-uint16_t Power_Time = 0; // Power on time.
-
-uint16_t Usart_Time = 0; // USART communication time.
-
-uint16_t ADC_Time = 0; // ADC sampling time.
-
-uint16_t Shutdown_Time_S = 0; // Shutdown time in seconds.
-
-uint8_t Shutdown_Time_M = 0; // Shutdown time in minutes.
-
-/*
-	ADC1
-*/
-float ADC1_Val = 0;
-/*
-	ADC2
-*/
-float ADC2_Val = 0;
-
-float Charge_Voltage = 0; // Charging Port Voltage
-
-/*
-	Light_Profile = 0; Default state.
-	Light_Profile = 1; Headlight bightness low, Lightpad brightness high, beep once
-	Light_Profile = 2; Headlight bightness mid, Lightpad brightness mid, beep twice
-	Light_Profile = 3; Headlight bightness high, Lightpad brightness low, beep thrice
-*/
-uint8_t Light_Profile = 0;
-
-uint8_t Lightbar_Brightness = 0; // Lightbar runtime brightness setting (0-255)
-
 uint8_t Buzzer_Frequency = 0; // Buzzer frequency in BPM, beats per minute, ranging from 60 to 180, corresponding to a duty cycle of 70 to 100)
 
-uint16_t Flashlight_Detection_Time = 0; // When the button is double-clicked without pressing it, the headlight stays on for 3 seconds.
-
-float Charge_Current = 0; // Charging Port Current
+//// CHARGE STATE
 
 /*
 	V_I = 0: Sampling charging current.
@@ -118,6 +107,23 @@ float Charge_Current = 0; // Charging Port Current
 */
 uint8_t V_I = 1;
 
-uint8_t Shutdown_Cnt = 0; // Number of times the current reaches the shutdown value.
+float Charge_Voltage = 0; // Charging Port Voltage
 
+float Charge_Current = 0; // Charging Port Current
+
+
+//// COUNTING FLAGS
+
+uint16_t Power_Time = 0; // Power on time.
+uint16_t Charge_Time = 0; // Charging time.
+uint16_t Usart_Time = 0; // USART communication time.
+
+uint16_t Flashlight_Time = 0; // Flashlight time.
+uint16_t Flashlight_Detection_Time = 0; // When the button is double-clicked without pressing it, the headlight stays on for 3 seconds.
+
+uint16_t ADC_Time = 0; // ADC sampling time.
+
+uint8_t Shutdown_Time_M = 0; // Shutdown time in minutes.
+uint16_t Shutdown_Time_S = 0; // Shutdown time in seconds.
+uint8_t Shutdown_Cnt = 0; // Number of times the current reaches the shutdown value.
 uint16_t Charger_Detection_1ms = 0; // Charger detection time.
--- Code/App/flag_bit.h
+++ Code/App/flag_bit.h
@@ -3,34 +3,51 @@
 
 #include "hk32f030m.h"
 
+// BOARD STATE
+
 extern uint8_t Power_Flag;
 extern uint8_t Charge_Flag;
+extern uint8_t Usart_Flag;
+
+// LIGHT STATE
+
 extern uint8_t Flashlight_Flag;
+extern uint8_t Light_Profile;
 extern uint8_t Brightness_Adjustment_Flag;
 extern uint8_t Lightbar_Battery_Flag;
 extern uint8_t Sensor_Activation_Display_Flag;
+extern uint8_t Lightbar_Brightness;
 extern uint8_t Power_Display_Flag;
-extern uint8_t Buzzer_Flag;
-extern uint8_t Usart_Flag;
-extern uint16_t Buzzer_Time;
-extern uint16_t Charge_Time;
-extern uint16_t Flashlight_Time;
-extern uint16_t Power_Time;
-extern uint16_t Usart_Time;
+
+// FOOTPAD STATE
+
 extern uint16_t ADC_Time;
-extern uint16_t Shutdown_Time_S;
-extern uint8_t Shutdown_Time_M;
 extern float ADC1_Val;
 extern float ADC2_Val;
-extern float Charge_Voltage;
-extern uint8_t Light_Profile;
-extern uint8_t Lightbar_Brightness;
+
+// BUZZER STATE
+
+extern uint8_t Buzzer_Flag;
+extern uint16_t Buzzer_Time;
 extern uint8_t Buzzer_Frequency;
-extern uint16_t Flashlight_Detection_Time;
-extern float Charge_Current;
+
+// CHARGE STATE
+
 extern uint8_t V_I;
-extern uint8_t Shutdown_Cnt;
+extern float Charge_Voltage;
+extern float Charge_Current;
+
+// COUNTING FLAGS
+
+extern uint16_t Power_Time;
+extern uint16_t Charge_Time;
+extern uint16_t Usart_Time;
 extern uint16_t Charger_Detection_1ms;
+extern uint16_t Flashlight_Time;
+extern uint16_t Flashlight_Detection_Time;
+extern uint16_t Shutdown_Time_S;
+extern uint8_t Shutdown_Time_M;
+extern uint8_t Shutdown_Cnt;
 #endif
 
 
